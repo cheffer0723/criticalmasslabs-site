8e6c721fbc74bdfd0293c6cc77a0c3b1a9934a50
Critical Mass Collective <cheff0723@outlook.com>
2026-08-03 20:18:30 -0400

    Redesign site with ASYMMETRY language

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const menuButton = document.querySelector('.menu-button');
-const navigation = document.querySelector('.nav');
-
-menuButton.addEventListener('click', () => {
-  const isOpen = navigation.classList.toggle('open');
-  menuButton.setAttribute('aria-expanded', String(isOpen));
+const button = document.querySelector('.menu-button');
+const menu = document.querySelector('.mobile-nav');
+button.addEventListener('click', () => {
+  const open = menu.classList.toggle('open');
+  button.setAttribute('aria-expanded', String(open));
 });
-
-navigation.querySelectorAll('a').forEach((link) => link.addEventListener('click', () => {
-  navigation.classList.remove('open');
-  menuButton.setAttribute('aria-expanded', 'false');
+menu.querySelectorAll('a').forEach((link) => link.addEventListener('click', () => {
+  menu.classList.remove('open');
+  button.setAttribute('aria-expanded', 'false');
 }));
-
 document.querySelector('#year').textContent = new Date().getFullYear();
